d0292f0c776f7fa9e29fa1dd56dac1723bada15e
nonnerdy <biswajiitjana@gmail.com>
2020-09-22 21:40:13 +0530

    added validation in Project pojo

--- ppmtool/src/main/java/com/biswajit/bootreact/domain/Project.java
+++ ppmtool/src/main/java/com/biswajit/bootreact/domain/Project.java
@@ -2,12 +2,17 @@ package com.biswajit.bootreact.domain;
 
 import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 @Entity
 public class Project {
@@ -15,19 +20,21 @@ public class Project {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+	@NotBlank(message = "Project name is required")
 	private String projectName;
-	
+	@NotBlank(message = "Project Identifier required")
+	@Size(min = 4,max = 5,message = "Please use 4 to 5 characters")
+	@Column(updatable = false,unique = true)
 	private String projectIdentifier;
-	
+	@NotBlank(message = "Project description is required")
 	private String description;
-	
+	@JsonFormat(pattern = "yyy-mm-dd")
 	private Date start_date;
-	
+	@JsonFormat(pattern = "yyy-mm-dd")
 	private Date end_date;
-	
+	@JsonFormat(pattern = "yyy-mm-dd")
 	private Date created_At;
-	
+	@JsonFormat(pattern = "yyy-mm-dd")
 	private Date updated_At;
 	
 	public Project() {
