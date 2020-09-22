0fd05bb7b7d576955632bd25dab46529e240460f
nonnerdy <biswajiitjana@gmail.com>
2020-09-22 20:38:40 +0530

    first commit

---
+++ ppmtool/src/test/java/com/biswajit/bootreact/PpmtoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biswajit.bootreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmtoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
